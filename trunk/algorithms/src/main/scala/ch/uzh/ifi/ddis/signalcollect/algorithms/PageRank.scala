3c6022e9628f65b5e68664ba26359726db0dfe04
philip.stutz@gmail.com <philip.stutz@gmail.com@0d1b279d-b6aa-0643-d918-6b9b84b704c3>
2011-03-14 12:54:16 +0000

    initial import of algorithms

---
+++ trunk/algorithms/src/main/scala/ch/uzh/ifi/ddis/signalcollect/algorithms/PageRank.scala
@@ -0,0 +1,75 @@
+/*
+ *  @author Philip Stutz
+ *  
+ *  Copyright 2010 University of Zurich
+ *      
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+
+package ch.uzh.ifi.ddis.signalcollect.algorithms
+
+import ch.uzh.ifi.ddis.signalcollect.api._
+import ch.uzh.ifi.ddis.signalcollect.api.vertices._
+import ch.uzh.ifi.ddis.signalcollect.api.edges._
+import ch.uzh.ifi.ddis.signalcollect.implementations.graph.SumOfOutWeights
+
+/** Represents an edge in a PageRank compute graph
+ * 
+ *  @param s: the identifier of the source vertex
+ *  @param t: the identifier of the target vertex
+ */
+class Link(s: Any, t: Any) extends DefaultEdge(s, t) {
+
+	type SourceVertexType <: SumOfOutWeights[Any, Double]
+	
+  /** The signal function calculates how much rank the source vertex
+   *  transfers to the target vertex. */
+  def signal = source.state.asInstanceOf[Double] * weight / source.sumOfOutWeights
+
+}
+
+/** Represents a page in a PageRank compute graph
+ * 
+ *  @param id: the identifier of this vertex
+ *  @param dampingFactor: @see <a href="http://en.wikipedia.org/wiki/PageRank">PageRank algorithm</a>
+ */
+class Page(id: Any, dampingFactor: Double) extends SignalMapVertex(id, 1 - dampingFactor) {
+	
+  /** The collect function calculates the rank of this vertex based on the rank
+   *  received from neighbors and the damping factor. */
+  def collect: Double = 1 - dampingFactor + dampingFactor * mostRecentSignals[Double].foldLeft(0.0)(_ + _)
+
+  override def scoreSignal: Double = {
+    lastSignalState match {
+      case None => 1
+      case Some(oldState) => (state - oldState).abs
+    }
+  }
+  
+}
+
+/** Builds a PageRank compute graph and executes the computation */
+object PageRank extends Application {
+  val cg = new AsynchronousComputeGraph()
+  cg.addVertex[Page](1, 0.85)
+  cg.addVertex[Page](2, 0.85)
+  cg.addVertex[Page](3, 0.85)
+  cg.addEdge[Link](1, 2)
+  cg.addEdge[Link](2, 1)
+  cg.addEdge[Link](2, 3)
+  cg.addEdge[Link](3, 2)
+  val stats = cg.execute()
+  println(stats)
+  cg.shutDown
+}
---
+++ trunk/algorithms/src/main/scala/ch/uzh/ifi/ddis/signalcollect/algorithms/Sssp.scala
@@ -0,0 +1,91 @@
+/*
+ *  @author Philip Stutz
+ *  
+ *  Copyright 2010 University of Zurich
+ *      
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+
+package ch.uzh.ifi.ddis.signalcollect.algorithms
+
+import ch.uzh.ifi.ddis.signalcollect.api._
+import ch.uzh.ifi.ddis.signalcollect.api.vertices._
+import ch.uzh.ifi.ddis.signalcollect.api.edges._
+
+
+/** Represents an edge in a Single-Source Shortest Path compute graph.
+ *  The edge weight corresponds to the length of the path represented by
+ *  this edge.
+ * 
+ *  @param s: the identifier of the source vertex
+ *  @param t: the identifier of the target vertex
+ */
+class Path(s: Any, t: Any) extends DefaultEdge(s, t) {
+
+  /** Specifies the type of the source vertex. 
+   *  This avoids type-checks/-casts, for example when accessing source.state.
+   */
+  type SourceVertexType = Location
+
+  /** The signal function calculates the distance of the shortest currently
+   *  known path from the SSSP source vertex which passes through the source
+   *  vertex of this edge. This is obviously the shortest distance to the vertex
+   *  where this edge starts plus the length of the path represented by this
+   *  edge (= the weight of this edge). 
+   */
+  def signal: Int = {
+    if (source.state != Int.MaxValue) {
+      source.state + weight.toInt
+    } else {
+      Int.MaxValue
+    }
+  }
+}
+
+/** Represents a location in a SSSP compute graph
+ * 
+ *  @param id: the identifier of this vertex
+ *  @param initialDIstance: the initial distance of this vertex to the source location
+ *  if the distance is Int.MaxValue this means that there is no known path. If the distance is
+ *  0 this means that this vertex is the source location.
+ */
+class Location(id: Any, initialDistance: Int) extends SignalMapVertex(id, initialDistance) {
+	
+  /** The collect function calculates shortest currently known path
+   *  from the source location. This is obviously either the shortest known path
+   *  up to now (= state) or one of the paths that had been advertised via a signal
+   *  by a neighbor. */
+  def collect: Int = mostRecentSignals[Int].foldLeft(state)(math.min(_, _))
+  
+}
+
+/** Builds a Single-Source Shortest Path compute graph and executes the computation */
+object SingleSourceShortestPath extends Application {
+  val cg = new AsynchronousComputeGraph()
+  cg.addVertex[Location](1, 0)
+  cg.addVertex[Location](2, Int.MaxValue)
+  cg.addVertex[Location](3, Int.MaxValue)
+  cg.addVertex[Location](4, Int.MaxValue)
+  cg.addVertex[Location](5, Int.MaxValue)
+  cg.addVertex[Location](6, Int.MaxValue)
+  cg.addEdge[Path](1, 2)
+  cg.addEdge[Path](2, 3)
+  cg.addEdge[Path](3, 4)
+  cg.addEdge[Path](1, 5)
+  cg.addEdge[Path](4, 6)
+  cg.addEdge[Path](5, 6)
+  val stats = cg.execute()
+  println(stats)
+  cg.shutDown
+}
